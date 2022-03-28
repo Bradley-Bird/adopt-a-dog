d66668553447c6fa995b8b2731d7e72b95bb6bb5
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-28 16:57:15 -0700

    link to detail page working, and detail page connected to supabase by id

---
+++ src/components/DogDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DogDetail({ dog: { name, bio, age, breed, image } }) {
+  return (
+    <div>
+      <img src={image} alt="" />
+      <h2>{name}</h2>
+      <p>age:{age}</p>
+      <p>breed: {breed}</p>
+      <p>{bio}</p>
+    </div>
+  );
+}
+
+export default DogDetail;
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import DogDetail from '../components/DogDetail';
+import { getDodById } from '../services/dogs';
+import { useParams } from 'react-router-dom';
 
 function Details() {
-  return <div>Details</div>;
+  const [dog, setDog] = useState({});
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDodById(params.id);
+      setDog(data);
+    };
+    fetchData();
+  }, [params]);
+
+  return (
+    <div>
+      <DogDetail {...{ dog }} />
+    </div>
+  );
 }
 
 export default Details;
