0c3471ca55f078ffc91f3ba3a75e95107a636856
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-29 12:21:37 -0700

    delete supabase call added, functionality added onClick of delete button on detail page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import DogDetail from '../components/DogDetail';
-import { getDogById } from '../services/dogs';
-import { useParams, Link } from 'react-router-dom';
+import { getDogById, deleteDog } from '../services/dogs';
+import { useParams, Link, useHistory } from 'react-router-dom';
 
 function Details() {
+  const history = useHistory();
   const [dog, setDog] = useState({});
   const params = useParams();
   useEffect(() => {
@@ -14,10 +15,18 @@ function Details() {
     fetchData();
   }, [params]);
 
+  const handleDelete = async () => {
+    await deleteDog(params.id);
+    history.push('/dogs');
+  };
+
   return (
     <div>
       <DogDetail {...{ dog }} />
-      <Link to={`${params.id}/edit`}>Edit</Link>
+      <Link to={`${params.id}/edit`}>
+        <button>Edit</button>
+      </Link>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
