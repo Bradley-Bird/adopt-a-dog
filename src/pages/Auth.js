4250693253b0ba37c92a344f5277385df3fb2a94
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-30 11:16:19 -0700

    state added for email, password

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
 function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div>
       <h4>
         <span>Sign In</span>
       </h4>
       <form action="">
-        <input type="email" name="" id="" placeholder="Email" />
-        <input type="password" name="" id="" placeholder="password" />
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Sign In</button>
       </form>
     </div>
   );
